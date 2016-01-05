f41bb8eecbae6a7b52468c5494157e1743000098
Daniel Barabash <daniil.barabash@pasante.cz>
2016-01-05 11:06:15 +0100

    added logging

--- src/main/java/com/template/controller/MainController.java
+++ src/main/java/com/template/controller/MainController.java
@@ -1,11 +1,14 @@
 package com.template.controller;
 
 import com.template.service.DummyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * main controller
@@ -13,11 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
+    /**
+     * default logger
+     */
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @Autowired
     DummyService dummyService;
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public ResponseEntity<String> test() {
+
+        logger.info("calling controller!");
+
         return ResponseEntity.ok(dummyService.callMe());
     }
 }
